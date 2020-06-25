3c350f3e80c04d2c35f704de75ecf39c6f9a14c1
DANIEL <daniel.quispe.o@tecsup.edu.pe>
2020-06-25 00:17:36 -0500

    first commit

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -8,7 +8,7 @@ export class ProductosService {
   url='http://localhost:3000/productos/'; 
   constructor(private http: HttpClient) { }
   listar() {
-    return this.http.get(`${this.url}listar`);
+    return this.http.get(`${this.url}`);
   }
   nuevo(producto) {
     return this.http.post(`${this.url}crear`, producto);    
